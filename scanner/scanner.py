ccf90ebdf0bc4cabdae43ce5dcb7dc61627f798d
Lubiekwadraty123 <stanislawblad02@gmail.com>
2025-07-21 14:50:47 +0200

    attributes

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -8,46 +8,42 @@ import http.server
 import socketserver
 import argparse
 import threading
-
+import _thread as thread
+import os
 
 # optional functionn to check if there is working camera port
 def list_ports():
-    """
-    Test the ports and returns a tuple with the available ports 
-    and the ones that are working.
-    """
-    is_working = True
-    dev_port = 0
-    working_ports = []
-    available_ports = []
-    while is_working:
-        print('opening port %s' %dev_port)
-        camera = cv2.VideoCapture(dev_port, cv2.CAP_DSHOW)
-        print('opened port %s' %dev_port)
-        if not camera.isOpened():
-            is_working = False
-            print("Port %s is not working." %dev_port)
-        else:
-            is_reading, img = camera.read()
-            w = camera.get(3)
-            h = camera.get(4)
-            if is_reading:
-                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
-                working_ports.append(dev_port)
+        """
+        Test the ports and returns a tuple with the available ports 
+        and the ones that are working.
+        """
+        is_working = True
+        dev_port = 0
+        working_ports = []
+        available_ports = []
+        while is_working:
+            print('opening port %s' %dev_port)
+            camera = cv2.VideoCapture(dev_port, cv2.CAP_DSHOW)
+            print('opened port %s' %dev_port)
+            if not camera.isOpened():
+                is_working = False
+                print("Port %s is not working." %dev_port)
             else:
-                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
-                available_ports.append(dev_port)
-        dev_port +=1
-    return available_ports,working_ports
+                is_reading, img = camera.read()
+                w = camera.get(3)
+                h = camera.get(4)
+                if is_reading:
+                    print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                    working_ports.append(dev_port)
+                else:
+                    print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                    available_ports.append(dev_port)
+            dev_port +=1
+        return available_ports,working_ports
 
-
-
-# Project using external usb webcam
-def main(port, mode):
+    # Project using external usb webcam
+def main(port, mode, client):
     print('start')
-    #connecting to mqtt protocol
-    client = mqtt.Client()
-    client.connect("localhost", 1883, 60)
     
     # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cap = cv2.VideoCapture(port, mode)
@@ -85,7 +81,7 @@ def main(port, mode):
         t = time.time()
         succes, img = cap.read()
 
-       
+    
         
         
         time_cap += time.time() - t
@@ -96,7 +92,7 @@ def main(port, mode):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             # img = cv2.GaussianBlur(img, (5, 5), 0)
             
-            cv2.imwrite("stream/image1.jpeg", img)
+            cv2.imwrite("tmp/stream1.jpeg", img)
 
             # decoding
             t = time.time()
@@ -109,7 +105,8 @@ def main(port, mode):
                     print(barcode.data)
                     myData = barcode.data.decode('utf-8')
                     print(myData)
-                    client.publish("scan/code", myData)
+                    if client != None:
+                        client.publish("scan/code", myData)
                     LastReadTime = currentTime
         else:
             print("image not captured")
@@ -119,6 +116,24 @@ def main(port, mode):
         # cv2.imshow('Result', img)
         # cv2.waitKey(1)
 
+
+        
+def run_http_server():
+    PORT 		= 8000
+    DIRECTORY 	= "web"
+    
+    Handler = http.server.SimpleHTTPRequestHandler
+    # creating stream folder
+    if not os.path.isdir(DIRECTORY+'/tmp'):
+        os.mkdir(DIRECTORY+'/tmp')
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=DIRECTORY, **kwargs)
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("Serving at port", PORT)
+            httpd.serve_forever()
+
+   
     
 
 
@@ -131,41 +146,51 @@ def main(port, mode):
 
 # list_ports()
 
-def run_http_server():
 
-	PORT 		= 8000
-	DIRECTORY 	= "web"
-	
-	# Handler = http.server.SimpleHTTPRequestHandler
-	
-	class Handler(http.server.SimpleHTTPRequestHandler):
-	    def __init__(self, *args, **kwargs):
-	        super().__init__(*args, directory=DIRECTORY, **kwargs)
-	
-	with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	    print("Serving at port", PORT)
-	    httpd.serve_forever()
 
-# parse CLI arguments
-parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument("foo", ..., required=True)
-# parser.add_argument('--http.server',action=argparse.BooleanOptionalAction)
-# parser.set_defaults(http=False)
-parser.add_argument('--http',action='store_true')
+
+    # if use_http == True :
+    #     run_http_server()
+    # thread = threading.Thread(None, run_http_server)
+    # thread.start()
+    
+  
+    # ... do things ...
+
+    # Shutdown server
+    # server.shutdown()
+    # thread.join()
+    
+
+    
+
+# Parse CLI arguments
+parser = argparse.ArgumentParser(
+    description="you can use 3 arguments like --list, --http, --mqtt"
+)
+parser.add_argument("--list", required=False, type=bool)
+parser.add_argument("--http", required=False, type=bool)
+parser.add_argument("--mqtt", required=False, type=str)
 args = parser.parse_args()
-
 print(args)
+if args.list == True:
+    list_ports()
+    exit()
 
-#if args.http == True :
-	# run_http_server()
-	thread = threading.Thread(None, run_http_server)
-	thread.start()
-	
+if args.http == True:
+    thread.start_new_thread(run_http_server, ())
 
-# ... do things ...
+client = None
+if args.mqtt != None: 
+    #connecting to mqtt protocol
+    client = mqtt.Client()
+    try:
+        client.connect(args.mqtt, 1883, 60)
+    except:
+        print('cannot connect to mqtt')
+        exit()
 
-# Shutdown server
-server.shutdown()
-thread.join()
-	
+main(0, cv2.CAP_DSHOW, client)
+# options(args.list, args.http, args.mqtt)
+
 	    
